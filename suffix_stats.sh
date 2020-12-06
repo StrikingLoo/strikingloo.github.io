0735a5afb57a11824ac056df9f8e5cc1ef6c8e0c
strikingloo <lucianostrika44@gmail.com>
2020-12-06 03:15:15 -0300

    improve wording in stats

--- suffix_stats.sh
+++ suffix_stats.sh
@@ -1,2 +1,9 @@
+echo 'Markdown lines: '
 find . -name "*.md"| xargs wc -l | tail -n 1
+echo 'Markdown word count: '
+find . -name "*.md"| xargs wc -w | tail -n 1
+echo '# of Markdown files: '
 find . -name "*.md"|wc -l
+
+
+
