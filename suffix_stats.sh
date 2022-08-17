4d31529df12e4680b10b14dfade4ea5fb281a236
strikingLoo <lucianostrika44@gmail.com>
2022-08-17 02:19:23 -0300

    make suffix stats work for new layout

--- suffix_stats.sh
+++ suffix_stats.sh
@@ -1,9 +1,9 @@
 echo 'Markdown lines: '
-find ./wiki-articles/ -name "*.md"| xargs wc -l | tail -n 1
+find ./wiki/ -name "*.md"| xargs wc -l | tail -n 1
 echo 'Markdown word count: '
-find ./wiki-articles/ -name "*.md"| xargs wc -w | tail -n 1
+find ./wiki/ -name "*.md"| xargs wc -w | tail -n 1
 echo '# of Markdown files: '
-find ./wiki-articles/ -name "*.md"|wc -l
+find ./wiki/ -name "*.md"|wc -l
 
 
 
