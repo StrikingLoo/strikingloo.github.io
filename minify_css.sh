536134c8db70d560021fff07a6af6bbbd630320d
strikingLoo <lucianostrika44@gmail.com>
2022-09-08 12:32:31 -0300

    very happy with how design looks now! Monochrome, mobile friendly with wide buttons and single line for desktop, all css is minified and joined to make a single request

--- minify_css.sh
+++ minify_css.sh
@@ -1,4 +1,8 @@
+cat main.css wiki.css > main-wiki.css
+cat main.css post.css > main-post.css
+cat main.css blog.css > main-blog.css
+
 cleancss -o main-min.css main.css
-cleancss -o wiki-min.css wiki.css
-cleancss -o post-min.css post.css
-cleancss -o blog-min.css blog.css
+cleancss -o wiki-min.css main-wiki.css
+cleancss -o post-min.css main-post.css
+cleancss -o blog-min.css main-blog.css
