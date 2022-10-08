d7622ca9dbb344e98c04b10d6070e797d68e5cf1
strikingLoo <lucianostrika44@gmail.com>
2022-10-08 01:16:57 -0300

    try new approach using command

--- add_header_links.sh
+++ add_header_links.sh
@@ -1,2 +1,18 @@
-sed -iE "s/\<h2 id=\"\(.*\)\"\>\(.*\)\<\/h2\>/\<h2 id=\"\1\"\>\<a href=\"#\1\"\>\2\<\/a\>\<\/h2\>/g" _site/wiki/*.html _site/*.html
-sed -iE "s/\<h3 id=\"\(.*\)\"\>\(.*\)\<\/h3\>/\<h3 id=\"\1\"\>\<a href=\"#\1\"\>\2\<\/a\>\<\/h3\>/g" _site/wiki/*.html _site/*.html
+#!/bin/bash
+for filename in ./_site/wiki/*.html; do
+    sed -iE "s/\<h2 id=\"\(.*\)\"\>\(.*\)\<\/h2\>/\<h2 id=\"\1\"\>\<a href=\"#\1\"\>\2\<\/a\>\<\/h2\>/g" "$filename"
+    echo "$filename" >> files_log.html
+done
+
+for filename in ./_site/*.html; do
+    sed -iE "s/\<h2 id=\"\(.*\)\"\>\(.*\)\<\/h2\>/\<h2 id=\"\1\"\>\<a href=\"#\1\"\>\2\<\/a\>\<\/h2\>/g" "$filename"
+    echo "$filename" >> files_log.html
+done
+
+for filename in ./_site/wiki/*.html; do
+    sed -iE "s/\<h3 id=\"\(.*\)\"\>\(.*\)\<\/h3\>/\<h3 id=\"\1\"\>\<a href=\"#\1\"\>\2\<\/a\>\<\/h3\>/g" "$filename"
+done
+
+for filename in ./_site/*.html; do
+    sed -iE "s/\<h3 id=\"\(.*\)\"\>\(.*\)\<\/h3\>/\<h3 id=\"\1\"\>\<a href=\"#\1\"\>\2\<\/a\>\<\/h3\>/g" "$filename"
+done
